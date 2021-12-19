8c5b242d76f36eaf866de24706292db91a580e77
AfnanAl97 <iAfnanAlharbi9@gmail.com>
2021-12-19 11:54:29 +0300

    Adding some design

--- travel-log/src/components/Post.js
+++ travel-log/src/components/Post.js
@@ -6,12 +6,33 @@ function Post() {
         <>
         <Header/>
         <div className="post">
-            <div className="post-title">
-                 <h1>Write a travel guide</h1>
-            </div>
-            <div className="post-form">
-                 <h6>Help fellow travelers by writing up your past itinerary.</h6>
-            </div>
+                 <h1 id="post-title">Write a travel guide</h1>
+                 <h6 id="post-form">Help fellow travelers by writing up your past itinerary.</h6>
+        
+            <form className="guide">
+                <input 
+                   className="input1"
+                   type="text"
+                   placeholder="Title"
+                   name="title"
+                //    value={text}
+                //    onChange={handleForm1}
+                />
+                <button 
+                   className="publish"
+                // onClick={handlePublish}
+                >
+                    Publish
+                </button>
+                <input 
+                   className="input2"
+                   type="text"
+                   placeholder="Tell us your experience..."
+                   name="title"
+                //    value={text}
+                //    onChange={handleForm2}
+                />
+            </form>
         </div>
         </>
     )
