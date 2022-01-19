326fb9cfdb7b844ee76bf6ba073b75825129ff0e
AfnanAl97 <iAfnanAlharbi9@gmail.com>
2022-01-19 15:34:57 +0300

    backend

--- travel-log/src/reducers/store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import postReducer from "./Post/reducer";
-import usersReducer from "./SignupLogin/reducer";
-
-const reducers = combineReducers({ postReducer, usersReducer })
-const store = createStore(reducers)
-
-export default store;
